f2ffe6738ce2ab31bbf681312c639daee0522014
Eauldane <elf@eauldane.com>
2026-02-03 23:14:17 +0000

    Add license headers

--- ElezenTools/Logging/Log.cs
+++ ElezenTools/Logging/Log.cs
@@ -1,5 +1,15 @@
+// SPDX-License-Identifier: AGPL-3.0-or-later
+// Copyright (C) 2026 Eauldane
+//
+// This file is part of ElezenTools.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Affero General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
 using ElezenTools.Services;
-using Serilog.Events;
+
 namespace ElezenTools.Logging;
 
 public static class Log
