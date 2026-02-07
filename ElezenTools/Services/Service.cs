7bc7e1182439a484a3f832eea0d0b5dfd786a567
Eauldane <elf@eauldane.com>
2026-02-07 02:47:35 +0000

    Framework stuff, PC and WI structs

--- ElezenTools/Services/Service.cs
+++ ElezenTools/Services/Service.cs
@@ -18,15 +18,50 @@ namespace ElezenTools.Services;
 
 #pragma warning disable S1118
 public class Service
-#pragma warning restore S1118
 {
     [PluginService] public static IDalamudPluginInterface PluginInterface { get; private set; }
     [PluginService] public static IFramework Framework { get; private set; }
     [PluginService] public static IPlayerState PlayerState { get; private set; }
     [PluginService] public static IPluginLog Log { get; private set; }
+    [PluginService] public static IObjectTable ObjectTable { get; private set; }
 
     public static void Init(IDalamudPluginInterface pluginInterface)
     {
         pluginInterface.Create<Service>();
+    }
+
+    public static async Task UseFramework(System.Action action)
+    {
+        Log.Information("Framework Event {0}", action.GetType().Name);
+
+        if (!Framework.IsInFrameworkUpdateThread)
+        {
+            await Framework.RunOnFrameworkThread(action).ContinueWith((_) => Task.CompletedTask).ConfigureAwait(false);
+            while (Framework.IsInFrameworkUpdateThread) // yield the thread again, should technically never be triggered
+            {
+                await Task.Delay(1).ConfigureAwait(false);
+            }
+        }
+        else
+        {
+            action();
+        }
+    }
+    
+    public static async Task<T> UseFramework<T>(Func<T> func)
+    {
+        if (!Framework.IsInFrameworkUpdateThread)
+        {
+            Log.Information("Framework Event {0}", typeof(T).Name);
+            var result = await Framework.RunOnFrameworkThread(func).ContinueWith((task) => task.Result).ConfigureAwait(false);
+            while (Framework.IsInFrameworkUpdateThread) // yield the thread again, should technically never be triggered
+            {
+                await Task.Delay(1).ConfigureAwait(false);
+            }
+            return result;
+        }
+        Log.Information("Framework Event - off thread {0}", typeof(T).Name);
+
+        return func.Invoke();
     }
 }
